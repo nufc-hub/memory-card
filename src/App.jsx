cb54ac39b5e2ac21ebe34b755625927f4d7cf243
KC <121441283+nufc-hub@users.noreply.github.com>
2025-09-04 22:30:23 +0900

    Add setActivePage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
+  const [activePage, setActivePage] = useState('home');
+
   return (
     <div>
-      <Header />
-      <Scoreboard />
+      {activePage === 'home' && (
+        // Homepage
+        <>
+          <Header />
+          <Scoreboard />
+          <Cards />
+          <Footer />
+        </>
+      )}
+
+      {
+        // References page
+        activePage === 'references' && <References />
+      }
     </div>
   );
 }
