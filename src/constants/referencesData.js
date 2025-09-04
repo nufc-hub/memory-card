34354b435c2fcc1e4cb53613f04980b984ebff04
KC <121441283+nufc-hub@users.noreply.github.com>
2025-09-04 21:38:51 +0900

    Create references data array

---
+++ src/constants/referencesData.js
@@ -0,0 +1,15 @@
+// Asset references data
+export const referencesData = [
+  {
+    id: crypto.randomUUID(),
+    url: 'https://www.flaticon.com/free-icons/melon',
+    title: 'melon icons',
+    description: 'Melon icons created by Freepik - Flaticon',
+  },
+  {
+    id: crypto.randomUUID(),
+    url: 'https://www.flaticon.com/free-icons/kimchi',
+    title: 'kimchi icons',
+    description: 'Kimchi icons created by Flat Icons',
+  },
+];
