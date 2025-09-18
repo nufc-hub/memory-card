855cf69eac27bc3926262666628427f58f5dc51d
KC <121441283+nufc-hub@users.noreply.github.com>
2025-09-18 17:39:46 +0900

    Import food data and refactor component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,16 @@
 import '../styles/Cards.css';
+import { foodData } from '../constants/cardData';
 
 export default function Cards() {
-  const cards = [
-    { id: 1, title: 'sushi' },
-    { id: 2, title: 'ramen' },
-    { id: 3, title: 'kimbap' },
-    { id: 4, title: 'teokk' },
-  ];
+  // Loop through the cardData array in constants/cardData.js instead of looping through this
+  const cards = foodData;
   return (
     <div className="cards">
       {cards.map((card) => (
-        <div key={card.id} className="card">
-          <p>{card.title}</p>
-        </div>
+        <button key={card.id} className="card">
+          <img src={card.url} alt="sushi" />
+          <p>{card.name}</p>
+        </button>
       ))}
     </div>
   );
