f2fe48f4ec6374a76d8169a1f526af75275a6644
KC <121441283+nufc-hub@users.noreply.github.com>
2025-09-23 22:48:28 +0900

    Add logic for which card-set is displayed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,17 +4,33 @@ import { foodData } from '../constants/cardData';
 import shuffleArray from '../utils/shuffleArray';
 
 export default function Cards({ level }) {
-  // Get foodData array and randomise its contents
-  const [cards, setCards] = useState(shuffleArray(foodData));
+  // Get foodData array for each level and randomise its contents
+  const levelOneCards = foodData.slice(0, 3); // Three cards
+  const levelTwoCards = foodData.slice(0, 6); // Sixe cards
+  const levelThreeCards = foodData.slice(0, 9); // Nine cards
+  const levelFourCards = foodData; // Twelve cards
 
-  // If level 1 show three cards
-  // level 2 six
-  // level 3 nine
-  // level 4 twelve
+  let cardLevel;
+  if (level === 1) cardLevel = levelOneCards;
+  else if (level === 2) cardLevel = levelTwoCards;
+  else if (level === 3) cardLevel = levelThreeCards;
+  else cardLevel = levelFourCards;
+
+  const [cards, setCards] = useState(shuffleArray(cardLevel));
+
+  (useState(() => {
+    setCards(shuffleArray(cardLevel));
+  }),
+    [level]);
+
   return (
     <div className="cards">
       {cards.map((card) => (
-        <button key={card.id} className="card">
+        <button
+          key={card.id}
+          className="card"
+          onClick={() => setCards(shuffleArray(cardLevel))} // Randomise cards after each click
+        >
           <div className="card-contents">
             <img src={card.url} alt="sushi" />
             <p>{card.name}</p>
