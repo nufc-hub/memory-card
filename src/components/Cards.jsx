7bf56a8fe4fdb25b8eb5ccc16ab70e0aeb51caa2
KC <121441283+nufc-hub@users.noreply.github.com>
2025-09-30 22:19:43 +0900

    Separate Cards/Card into child parent component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,18 @@
 import '../styles/Cards.css';
 import { useState } from 'react';
 import { foodData } from '../constants/cardData';
+import Card from './Card';
 import shuffleArray from '../utils/shuffleArray';
 
 export default function Cards({ level }) {
   // Get foodData array for each level and randomise its contents
   const levelOneCards = foodData.slice(0, 3); // Three cards
-  const levelTwoCards = foodData.slice(0, 6); // Sixe cards
+  const levelTwoCards = foodData.slice(0, 6); // Six cards
   const levelThreeCards = foodData.slice(0, 9); // Nine cards
   const levelFourCards = foodData; // Twelve cards
+
+  // How to track which cards have been clicked?
+  // Read odin useEffect
 
   let cardLevel;
   if (level === 1) cardLevel = levelOneCards;
@@ -26,16 +30,13 @@ export default function Cards({ level }) {
   return (
     <div className="cards">
       {cards.map((card) => (
-        <button
+        <Card
           key={card.id}
-          className="card"
+          id={card.id}
+          name={card.name}
+          url={card.url}
           onClick={() => setCards(shuffleArray(cardLevel))} // Randomise cards after each click
-        >
-          <div className="card-contents">
-            <img src={card.url} alt="sushi" />
-            <p>{card.name}</p>
-          </div>
-        </button>
+        />
       ))}
     </div>
   );
