8542fc78d9e789c0dec37087079661699f6c744f
KC <121441283+nufc-hub@users.noreply.github.com>
2025-10-02 17:33:17 +0900

    Add logic for rendering end game screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import Cards from './components/Cards';
+import EndGameScreen from './components/EndGameScreen';
 import Footer from './components/Footer';
+import References from './components/References';
 
 function App() {
-  const [activePage, setActivePage] = useState('home');
+  const [activePage, setActivePage] = useState('endGame'); // Switches between different pages on site
+  const [level, setLevel] = useState(1); // Sets the game level, i.e. how many cards shown
+  const [currentScore, setCurrentScore] = useState(0); // This may go in scoreboard component
+  const [bestScore, setBestScore] = useState(0); // This may go in scoreboard component
 
   return (
     <div>
@@ -14,15 +19,26 @@ function App() {
         // Homepage
         <>
           <Header />
-          <Scoreboard />
-          <Cards />
-          <Footer />
+          <Scoreboard currentScore={currentScore} bestScore={bestScore} />
+          <Cards level={level} setLevel={setLevel} />
+          <Footer setActivePage={setActivePage} />
+        </>
+      )}
+
+      {activePage === 'endGame' && (
+        // End game screen
+        <>
+          <Header />
+          <Scoreboard currentScore={currentScore} bestScore={bestScore} />
+          <EndGameScreen currentScore={currentScore} />
         </>
       )}
 
       {
         // References page
-        activePage === 'references' && <References />
+        activePage === 'references' && (
+          <References setActivePage={setActivePage} />
+        )
       }
     </div>
   );
