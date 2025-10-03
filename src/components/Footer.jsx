5ac0ed06fe9bf33c36e37e6b14b376a6e5f85556
KC <121441283+nufc-hub@users.noreply.github.com>
2025-10-03 10:31:53 +0900

    Reset game when references button is clicked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import '../styles/Footer.css';
 
-export default function Footer({ setActivePage }) {
+export default function Footer({ setActivePage, resetGame }) {
   return (
     <div className="footer">
       {/* Links to references page */}
       <button
-        onClick={() => setActivePage('references')}
+        onClick={() => {
+          setActivePage('references');
+          resetGame();
+        }}
         className="references-button"
       >
         References
