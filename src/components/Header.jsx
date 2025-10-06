677c84af3d374479eab84ae786e20ad154b85b4c
KC <121441283+nufc-hub@users.noreply.github.com>
2025-10-06 14:11:32 +0900

    Add h1 element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import '../styles/Header.css';
 
 export default function Header() {
   return (
-    <header className="header">
-      <div>
-        <div className="logo">memory game</div>
+    <header className="main-header">
+      <div className="logo-container">
+        <div className="logo">
+          <h1>memory game</h1>
+        </div>
       </div>
     </header>
   );
