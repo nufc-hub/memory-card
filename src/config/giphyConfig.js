10a53f9d8b842f590947fd82d928245ca164ac97
KC <121441283+nufc-hub@users.noreply.github.com>
2025-10-11 13:01:31 +0900

    Add api key and gif id's

---
+++ src/config/giphyConfig.js
@@ -0,0 +1,16 @@
+export const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
+
+export const GIF_IDS = [
+  'Zq8d02zEzp6WsKRTI1', // kimchi
+  'ylcGy3ycduKBy4ovQf', // songpyeon
+  'okP0JJKfHvXA6tH9vO', // bibimbap
+  'tK8kUBZSU0nSMlIATw', // daege
+  'efOL9G7GKVRK6yH5iD', // naengmyeon
+  '3UQLWLfHfw13uR94uE', // tteokbokki
+  '7m9tqtFBrBQpEVisEm', // samgyeopsal
+  'GFHziwj4BPRW77DlCQ', // seolleongtang
+  'Is64liWk6HfP2xjJ3t', // jeon
+  'PKXTFWXoFivQAgZ7Sn', // dalkgalbi
+  'ixe7JckdpY7cOqOLq6', // jiggae
+  'N0FwVFDfY72EtbcYoV', // dasik
+];
