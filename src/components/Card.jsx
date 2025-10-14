bfcddd94c1dd9f42435b5ec86d3b5c72994d9e45
KC <121441283+nufc-hub@users.noreply.github.com>
2025-10-14 19:31:11 +0900

    Change prop name from 'name' to 'title'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import '../styles/Card.css';
 
-export default function Card({ name, url, onClick }) {
+export default function Card({ title, url, onClick }) {
   return (
     <button className="card" onClick={onClick}>
       <div className="card-contents">
-        <img src={url} alt={name} />
-        <p>{name}</p>
+        <img src={url} alt={title} />
+        <p>{title}</p>
       </div>
     </button>
   );
