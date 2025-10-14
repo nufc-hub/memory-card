b6986b29015c346c256255ed2d07f0c9a2ca9376
KC <121441283+nufc-hub@users.noreply.github.com>
2025-10-14 21:19:39 +0900

    Replace references element with giphy elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,26 @@ import githubIcon from '../assets/links/github/github-mark/github-mark.svg';
 
 export default function Footer() {
   return (
-    <footer className="main-footer">
-      {/* Links to references page */}
-      <a className="references-link">references</a>
+    <footer className="footer">
+      <p>
+        GIFs powered by {/* Link to giphy page */}
+        <a
+          href="https://giphy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="giphy-link"
+        >
+          Giphy
+        </a>
+      </p>
+      {/* Link to github page */}
       <a
         href="https://github.com/nufc-hub"
-        target="blank"
+        target="_blank"
         rel="noopener noreferrer"
         className="github-link"
       >
-        <img src={githubIcon} alt="GitHub" className="main-footer-img" />
+        <img src={githubIcon} alt="GitHub" className="footer-img" />
         <p>nufc</p>
       </a>
     </footer>
